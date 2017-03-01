51d7d0f74d563985d68b59cb076b137e10e73986
Márk <damatusz@root@damatusz.ddns.net>
2017-03-01 01:03:21 +0100

    merged two migrations into one

--- database/migrations/2017_02_28_232952_create_products_table.php
+++ database/migrations/2017_02_28_232952_create_products_table.php
@@ -13,12 +13,21 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->increments('id');
-      		$table->string('name');
-      		$table->integer('price');
-      		$table->text('short_description');
-      		$table->text('description');
+		$table->increments('id');
+      		$table->string('name')->default('');
+      		$table->integer('price')->default(0);
+      		$table->text('short_description')->default('');
+      		$table->text('description')->default('');
         });
+
+	Schema::create('products_images', function(Blueprint $table) {
+		$table->increments('id');
+		$table->integer('product_id')->unsigned()->default(0);
+		$table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+		$table->string('name')->default('');
+		$table->string('url')->default('');
+	});
+
     }
 
     /**
@@ -28,6 +37,7 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
+	Schema::drop('products_images');
         Schema::drop('products');
     }
 }
